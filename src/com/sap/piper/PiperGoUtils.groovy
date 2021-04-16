c140bd2eca36c7559ae05eca0f3a1dd94943769d
d.small@sap.com <d.small@sap.com>
2021-04-16 18:40:55 +0000

    Download piper executable from dee0 releases.

--- src/com/sap/piper/PiperGoUtils.groovy
+++ src/com/sap/piper/PiperGoUtils.groovy
@@ -47,8 +47,8 @@ class PiperGoUtils implements Serializable {
                 }
             }
 
-            def fallbackUrl = 'https://github.com/SAP/jenkins-library/releases/latest/download/piper_master'
-            def piperBinUrl = (version == 'master') ? fallbackUrl : "https://github.com/SAP/jenkins-library/releases/download/${version}/piper"
+            def fallbackUrl = 'https://github.com/dee0/jenkins-library/releases/latest/download/piper_master'
+            def piperBinUrl = (version == 'master') ? fallbackUrl : "https://github.com/dee0/jenkins-library/releases/download/${version}/piper"
 
             boolean downloaded = downloadGoBinary(piperBinUrl)
             if (!downloaded) {
